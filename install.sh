d5835f4c2241411295cc1c7a8488ebcd4ef56d06
Aaron Saikovski <asaikovski@outlook.com>
2021-03-15 09:54:05 +1100

    added new installers

--- install.sh
+++ install.sh
@@ -4,11 +4,16 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 
-brew install --cask visual-studio-code
-brew install --cask terraform
-brew install --cask gimp
-brew install --cask taskmate
-#brew install p7zip
+brew install visual-studio-code
+brew install terraform
+brew install gimp
+brew install taskmate
+brew install p7zip
+brew install firefox
+brew install google-chrome
+brew install sublime-text
+brew install adobe-acrobat-reader
+brew install mysqlworkbench
 
 #update homebrew packages
 brew upgrade
